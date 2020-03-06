9c8178511017b91914a1b3bfb999adff8e032ef5
Shahzaib Hassan <61833621+Shahzaib096@users.noreply.github.com>
2020-03-06 16:48:28 +0500

    Add files via upload

---
+++ A Very Big Sum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Very_Big_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Enter Array Size
+            Console.Write("Enter Array Size : ");
+            long size = long.Parse(Console.ReadLine());
+            //input Array Elements
+            long[] array = new long[size];
+            Console.WriteLine("Enter ARray Elements");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = long.Parse(Console.ReadLine());
+
+            }
+            //Sum Of Array
+            long sum = array.Sum();
+
+            Console.WriteLine("Array Sum is :" + sum);
+            Console.Read();
+        }
+    }
+}
